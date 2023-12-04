11d73c5f0b2289e54632025e70520def5a75b853
Jakub <kuba.r.pawlowski@gmail.com>
2023-12-04 13:28:06 +0100

    Fixed null pointer exception by try catching it

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
         //write your code here
-        if (password.equals(repeatPassword) && password.length() >= 10) {
-        }else {
+        try{
+            if (!password.equals(repeatPassword) || password.length() < 10) {
+                throw new PasswordValidationException("Wrong passwords");
+            }
+        } catch (NullPointerException e){
             throw new PasswordValidationException("Wrong passwords");
         }
     }
