0656e665c4a694d687ab4d02db7651b3976db40a
Jakub <kuba.r.pawlowski@gmail.com>
2023-12-05 01:41:26 +0100

    Fixed null pointer exception by try catching it

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
+
 public class PasswordValidationException extends Exception {
-    //create your exception
+
     public PasswordValidationException(String message) {
         super(message); // Wywołanie konstruktora nadklasy z niestandardowym komunikatem
     }
--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         //write your code here
-        try{
+        try {
             if (!password.equals(repeatPassword) || password.length() < 10) {
                 throw new PasswordValidationException("Wrong passwords");
             }
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
